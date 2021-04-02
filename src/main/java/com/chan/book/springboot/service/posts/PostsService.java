07014aaee993a0e38d65dd7b3f47c40fcba6d1ad
chanyoung <young9484825@gmail.com>
2021-04-02 14:11:13 +0900

    PostsApiController 테스트

--- src/main/java/com/chan/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/chan/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.chan.book.springboot.service.posts;
 
+import com.chan.book.springboot.domain.posts.Posts;
 import com.chan.book.springboot.domain.posts.PostsRepository;
+import com.chan.book.springboot.web.dto.PostsResponseDto;
 import com.chan.book.springboot.web.dto.PostsSaveRequestDto;
+import com.chan.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        return new PostsResponseDto(entity);
     }
 }
